93df7ee3b391c8d1ede56eec8652965dafb8c3eb
maxhougas <max.hougas@gmail.com>
2022-07-28 14:22:07 -0400

    added failure codes

--- hougasconsts.h
+++ hougasconsts.h
@@ -2,10 +2,11 @@
 #define SUCCESS 0 //function worked as intended
 #define FAILURE 1 //unspecified failure
 #define NULL_STRUCTURE 2 //passed an unitialized pointer
-#define INVALID_MODE 3 //mode value not supported by function
-#define INVALID_DATATYPE 4 //data type value not supported by function
-#define EMPTY_SET 5 //attempted to operate on empty set
-#define MALLOC_FAIL //malloc failed to allocate memory
+#define IVALID_VALUE 3 //uspecified invalid value passed to function
+#define INVALID_MODE 4 //mode value not supported by function
+#define INVALID_DATATYPE 5 //data type value not supported by function
+#define EMPTY_SET 6 //attempted to operate on empty set
+#define MALLOC_FAIL 7 //malloc failed to allocate memory
 
 //Numerical data type
 #define INT 0
