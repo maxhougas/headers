ddb5d8653d260a3af4d1edceced6e5098ce70aa6
maxhougas <max.hougas@gmail.com>
2022-07-30 02:27:32 -0400

    enforced ANSI compliance in consts and vectors

--- hougasconsts.h
+++ hougasconsts.h
@@ -1,14 +1,14 @@
-//Return values for vector math
-#define SUCCESS 0 //function worked as intended
-#define FAILURE 1 //unspecified failure
-#define NULL_STRUCTURE 2 //passed an unitialized pointer
-#define INVALID_VALUE 3 //uspecified invalid value passed to function
-#define INVALID_MODE 4 //mode value not supported by function
-#define INVALID_DATATYPE 5 //data type value not supported by function
-#define EMPTY_SET 6 //attempted to operate on empty set
-#define MALLOC_FAIL 7 //malloc failed to allocate memory
+/*Return values for vector math*/
+#define SUCCESS 0 /*function worked as intended*/
+#define FAILURE 1 /*unspecified failure*/
+#define NULL_STRUCTURE 2 /*passed an unitialized pointer*/
+#define INVALID_VALUE 3 /*uspecified invalid value passed to function*/
+#define INVALID_MODE 4 /*mode value not supported by function*/
+#define INVALID_DATATYPE 5 /*data type value not supported by function*/
+#define EMPTY_SET 6 /*attempted to operate on empty set*/
+#define MALLOC_FAIL 7 /*malloc failed to allocate memory*/
 
-//Numerical data type
+/*Numerical data type*/
 #define INT 0
 #define FLO 1
 #define DOU 2
--- hougasvectors.h
+++ hougasvectors.h
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<hougasconsts.h>
+#include <stdio.h>
+#include "./hougasconsts.h"
 
-//Modes for parallel math
+/*Modes for parallel math*/
 #define ADD 0
 #define SUB 1
 #define MUL 2
@@ -25,108 +25,108 @@ int vectorParallelMath(void* result, void* a, void* b, int size, int mode, int d
     case INT:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] + ((int*)b)[i];
         break;
-        case SUB: //subtract
+        case SUB:
           for(i=0;i<size;i++)
            ((int*)result)[i] == ((int*)a)[i] - ((int*)b)[i];
         break;
-        case MUL: //multiply
+        case MUL:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] * ((int*)b)[i];
         break;
-        case DIV: //divide
+        case DIV:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] / ((int*)b)[i];
         break;
-        case MOD: //remainder
+        case MOD:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] % ((int*)b)[i];
         break;
-        case LOD: //copies a to result
+        case LOD:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i];
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     break;
     case FLO:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           for(i=0;i<size;i++)
             ((float*)result)[i] == ((float*)a)[i] + ((float*)b)[i];
         break;
-        case SUB: //subtract
+        case SUB:
           for(i=0;i<size;i++)
            ((float*)result)[i] == ((float*)a)[i] - ((float*)b)[i];
         break;
-        case MUL: //multiply
+        case MUL:
           for(i=0;i<size;i++)
             ((float*)result)[i] == ((float*)a)[i] * ((float*)b)[i];
         break;
-        case DIV: //divide
+        case DIV:
           for(i=0;i<size;i++)
             ((float*)result)[i] == ((float*)a)[i] / ((float*)b)[i];
         break;
-        case LOD: //copies a to result
+        case LOD:
           for(i=0;i<size;i++)
             ((float*)result)[i] == ((float*)a)[i];
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     break;
     case DOU:
     switch(mode)
       {
-        case ADD: //add
+        case ADD:
           for(i=0;i<size;i++)
             ((double*)result)[i] == ((double*)a)[i] + ((double*)b)[i];
         break;
-        case SUB: //subtract
+        case SUB:
           for(i=0;i<size;i++)
            ((double*)result)[i] == ((double*)a)[i] - ((double*)b)[i];
         break;
-        case MUL: //multiply
+        case MUL:
           for(i=0;i<size;i++)
             ((double*)result)[i] == ((double*)a)[i] * ((double*)b)[i];
         break;
-        case DIV: //divide
+        case DIV:
           for(i=0;i<size;i++)
             ((double*)result)[i] == ((double*)a)[i] / ((double*)b)[i];
         break;
-        case LOD: //copies a to result
+        case LOD:
           for(i=0;i<size;i++)
             ((double*)result)[i] == ((double*)a)[i];
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     break;
     case CHA:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           for(i=0;i<size;i++)
             ((char*)result)[i] == ((char*)a)[i] + ((char*)b)[i];
         break;
-        case SUB: //subtract
+        case SUB:
           for(i=0;i<size;i++)
            ((char*)result)[i] == ((char*)a)[i] - ((char*)b)[i];
         break;
-        case MUL: //multiply
+        case MUL:
           for(i=0;i<size;i++)
             ((char*)result)[i] == ((char*)a)[i] * ((char*)b)[i];
         break;
-        case DIV: //divide
+        case DIV:
           for(i=0;i<size;i++)
             ((char*)result)[i] == ((char*)a)[i] / ((char*)b)[i];
         break;
-        case MOD: //remainder
+        case MOD:
       }
     default:
-      ret = INVALID_DATATYPE; //invalid data type;
+      ret = INVALID_DATATYPE;
   }
 
   return ret;
@@ -147,113 +147,113 @@ int vectorImmediateMath(void* result, void* a, void* b, int size, int mode, int 
     case INT:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] + *((int*)b);
         break;
-        case SUB: //subtract
+        case SUB:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] - *((int*)b);
         break;
-        case MUL: //multiply
+        case MUL:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] * *((int*)b);
         break;
-        case DIV: //divide
+        case DIV:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] / *((int*)b);
         break;
-        case MOD: //remainder
+        case MOD:
           for(i=0;i<size;i++)
             ((int*)result)[i] == ((int*)a)[i] % *((int*)b);
         break;
-        case LOD: //copies a to result
+        case LOD:
           for(i=0;i<size;i++)
             ((int*)result)[i] == *((int*)b);
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     case FLO:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           for(i=0;i<size;i++)
             ((float*)result)[i] == ((float*)a)[i] + *((float*)b);
         break;
-        case SUB: //subtract
+        case SUB:
           for(i=0;i<size;i++)
             ((float*)result)[i] == ((float*)a)[i] - *((float*)b);
         break;
-        case MUL: //multiply
+        case MUL:
           for(i=0;i<size;i++)
             ((float*)result)[i] == ((float*)a)[i] * *((float*)b);
         break;
-        case DIV: //divide
+        case DIV:
           for(i=0;i<size;i++)
             ((float*)result)[i] == ((float*)a)[i] / *((float*)b);
         break;
-        case LOD: //copies a to result
+        case LOD:
           for(i=0;i<size;i++)
             ((float*)result)[i] == *((float*)b);
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     case DOU:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           for(i=0;i<size;i++)
             ((double*)result)[i] == ((double*)a)[i] + *((double*)b);
         break;
-        case SUB: //subtract
+        case SUB:
           for(i=0;i<size;i++)
             ((double*)result)[i] == ((double*)a)[i] - *((double*)b);
         break;
-        case MUL: //multiply
+        case MUL:
           for(i=0;i<size;i++)
             ((double*)result)[i] == ((double*)a)[i] * *((double*)b);
         break;
-        case DIV: //divide
+        case DIV:
           for(i=0;i<size;i++)
             ((double*)result)[i] == ((double*)a)[i] / *((double*)b);
         break;
-        case LOD: //copies a to result
+        case LOD:
           for(i=0;i<size;i++)
             ((double*)result)[i] == *((double*)b);
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     case CHA:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           for(i=0;i<size;i++)
             ((char*)result)[i] == ((char*)a)[i] + *((char*)b);
         break;
-        case SUB: //subtract
+        case SUB:
           for(i=0;i<size;i++)
             ((char*)result)[i] == ((char*)a)[i] - *((char*)b);
         break;
-        case MUL: //multiply
+        case MUL:
           for(i=0;i<size;i++)
             ((char*)result)[i] == ((char*)a)[i] * *((char*)b);
         break;
-        case DIV: //divide
+        case DIV:
           for(i=0;i<size;i++)
             ((char*)result)[i] == ((char*)a)[i] / *((char*)b);
         break;
-        case MOD: //remainder
+        case MOD:
           for(i=0;i<size;i++)
             ((char*)result)[i] == ((char*)a)[i] % *((char*)b);
         break;
-        case LOD: //copies a to result
+        case LOD:
           for(i=0;i<size;i++)
             ((char*)result)[i] == *((char*)b);
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     default:
-      ret = INVALID_DATATYPE; //invalid data type
+      ret = INVALID_DATATYPE;
   }
 
     return ret;
@@ -273,109 +273,109 @@ int vectorReduce(void* result, void* a, int size, int mode, int dattype)
     case INT:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           *(int*)result = 0;
           for(i=0;i<size;i++)
             *(int*)result += ((int*)a)[i];
         break;
-        case SUB: //subtract
+        case SUB:
           *(int*)result = 0;
           for(i=0;i<size;i++)
             *(int*)result -= ((int*)a)[i];
         break;
-        case MUL: //multiply
+        case MUL:
           *(int*)result = 1;
           for(i=0;i<size;i++)
             *(int*)result *= ((int*)a)[i];
         break;
-        case DIV: //divide
+        case DIV:
           *(int*)result = 1;
           for(i=0;i<size;i++)
             *(int*)result /= ((int*)a)[i];
         break;
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     break;
     case FLO:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           *(float*)result = 0;
           for(i=0;i<size;i++)
             *(float*)result += ((float*)a)[i];
         break;
-        case SUB: //subtract
+        case SUB:
           *(float*)result = 0;
           for(i=0;i<size;i++)
             *(float*)result -= ((float*)a)[i];
         break;
-        case MUL: //multiply
+        case MUL:
           *(float*)result = 1;
           for(i=0;i<size;i++)
             *(float*)result *= ((float*)a)[i];
         break;
-        case DIV: //divide
+        case DIV:
           *(float*)result = 1;
           for(i=0;i<size;i++)
             *(float*)result /= ((float*)a)[i];
         break;
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     break;
     case DOU:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           *(double*)result = 0;
           for(i=0;i<size;i++)
             *(double*)result += ((double*)a)[i];
         break;
-        case SUB: //subtract
+        case SUB:
           *(double*)result = 0;
           for(i=0;i<size;i++)
             *(double*)result -= ((double*)a)[i];
         break;
-        case MUL: //multiply
+        case MUL:
           *(double*)result = 1;
           for(i=0;i<size;i++)
             *(double*)result *= ((double*)a)[i];
         break;
-        case DIV: //divide
+        case DIV:
           *(double*)result = 1;
           for(i=0;i<size;i++)
             *(double*)result /= ((double*)a)[i];
         break;
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     break;
     case CHA:
       switch(mode)
       {
-        case ADD: //add
+        case ADD:
           *(char*)result = 0;
           for(i=0;i<size;i++)
             *(char*)result += ((char*)a)[i];
         break;
-        case SUB: //subtract
+        case SUB:
           *(char*)result = 0;
           for(i=0;i<size;i++)
             *(char*)result -= ((char*)a)[i];
         break;
-        case MUL: //multiply
+        case MUL:
           *(char*)result = 1;
           for(i=0;i<size;i++)
             *(char*)result *= ((char*)a)[i];
         break;
-        case DIV: //divide
+        case DIV:
           *(char*)result = 1;
           for(i=0;i<size;i++)
             *(char*)result /= ((char*)a)[i];
         break;
         default:
-          ret = INVALID_MODE; //invalid mode
+          ret = INVALID_MODE;
       }
     break;
   }
@@ -387,7 +387,7 @@ int vectorReduce(void* result, void* a, int size, int mode, int dattype)
 Print vector to terminal
 ***/
 
-int printVector(void* toprint, int size, int dattype)
+int vectorPrint(void* toprint, int size, int dattype)
 {
   int ret = SUCCESS;
 
@@ -399,25 +399,25 @@ int printVector(void* toprint, int size, int dattype)
     case INT:
       for(i=0;i<size-1;i++)
         printf("%d,",((int*)toprint)[i]);
-      printf("%d",((int*)toprint)[size]);
+      printf("%d",((int*)toprint)[size-1]);
     break;
     case FLO:
       for(i=0;i<size-1;i++)
         printf("%f,",((float*)toprint)[i]);
-      printf("%f",((float*)toprint)[size]);
+      printf("%f",((float*)toprint)[size-1]);
     break;
     case DOU:
       for(i=0;i<size-1;i++)
         printf("%f,",((double*)toprint)[i]);
-      printf("%f",((double*)toprint)[size]);
+      printf("%f",((double*)toprint)[size-1]);
     break;
     case CHA:
       for(i=0;i<size-1;i++)
         printf("%d,",((char*)toprint)[i]);
-      printf("%d",((char*)toprint)[size]);
+      printf("%d",((char*)toprint)[size-1]);
     break;
     default:
-    ret = INVALID_DATATYPE; //invalid data type
+    ret = INVALID_DATATYPE;
   }
 
   printf(">\n");
